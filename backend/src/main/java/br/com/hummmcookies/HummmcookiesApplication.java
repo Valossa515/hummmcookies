7a66ced428229a9812aeeb190b72deb13e3aefa6
Felipe Martins <fe.mmo515@gmail.com>
2021-09-13 16:09:59 -0300

    Project created

---
+++ backend/src/main/java/br/com/hummmcookies/HummmcookiesApplication.java
@@ -0,0 +1,13 @@
+package br.com.hummmcookies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HummmcookiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HummmcookiesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/hummmcookies/HummmcookiesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hummmcookies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HummmcookiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
